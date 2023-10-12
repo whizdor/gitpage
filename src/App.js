57862b85ab9258cc33e12567703cad79f844c5e2
whizdor <118841840+whizdor@users.noreply.github.com>
2023-10-12 09:44:47 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ReactDom from 'react-dom';
+import "./App.css";
+import Home from "./Home";
+import Project from "./Project_Details";
+import {Route, Routes, Router} from 'react-router-dom';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route export path = "/" element = {<Home />} />
+        <Route export path = "/projects" element = {<Project />} />
+      </Routes>
     </div>
   );
 }
