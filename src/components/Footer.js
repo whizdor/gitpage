13a50f7a6f0ee04b70acac29378211a3ca07b143
whizdor <118841840+whizdor@users.noreply.github.com>
2023-10-24 02:17:21 +0530

    hi

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,14 +10,13 @@ export const Footer = () => {
     <footer className="footer">
       <Container>
         <Row className="align-items-center">
-          <Col size={12} sm={6}>
+          <Col size={12} sm={6} className="align-items-center">
             <img src={logo} alt="Logo" className="Logo" />
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
-              <a href="#"><img src={navIcon1} alt="Icon" /></a>
-              <a href="#"><img src={navIcon2} alt="Icon" /></a>
-              <a href="#"><img src={navIcon3} alt="Icon" /></a>
+              <a href="https://www.linkedin.com/company/gdsc-iit-kanpur/" target="_blank" rel="noopener noreferrer"><img src={navIcon1} alt="Icon" /></a>
+              <a href="https://www.instagram.com/gdsc.iitk/" target="_blank" rel="noopener noreferrer"><img src={navIcon3} alt="Icon" /></a>
             </div>
             <p>Indian Institute of Technology, Kanpur</p>
             <p>&copy; {currentYear} GDSC IIT Kanpur. All rights reserved.</p>
